8769ec3b880eac7cb3d1b9ccbbd55d0d3398d973
FelipeMGAraujo <168040742+FelipeMGAraujo@users.noreply.github.com>
2025-11-24 20:10:35 -0300

    Criação das Classes Mesa e Cliente

---
+++ src/main/java/com/grupo_5/pub/Repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.grupo_5.pub.Repository;
+
+import com.grupo_5.pub.Model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
