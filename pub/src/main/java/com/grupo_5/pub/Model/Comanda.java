6e75968a74eb185cd7bfe7d73ed7a15df1a385b3
renatobarr <168466657+renatobarr@users.noreply.github.com>
2025-11-25 21:39:35 -0300

    🔧 Alterar o tipo do campo id de Long para Integer na entidade Comanda

--- pub/src/main/java/com/grupo_5/pub/Model/Comanda.java
+++ pub/src/main/java/com/grupo_5/pub/Model/Comanda.java
@@ -11,7 +11,7 @@ public class Comanda {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "id_cliente")
@@ -37,4 +37,60 @@ public class Comanda {
     private List<PromocaoAplicada> promocoes;
 
     // GETTERS E SETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDateTime getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDateTime dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public BigDecimal getValorSubtotal() {
+        return valorSubtotal;
+    }
+
+    public void setValorSubtotal(BigDecimal valorSubtotal) {
+        this.valorSubtotal = valorSubtotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
