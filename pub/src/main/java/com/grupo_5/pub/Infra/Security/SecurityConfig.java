a8da2020e5124bd7eda964b2935a9c364fbb346d
Ana Villas <anacarolina.vbp@gmail.com>
2026-03-04 00:21:40 -0300

    🐛 fix: remove misplaced semicolons in SecurityConfig

--- pub/src/main/java/com/grupo_5/pub/Infra/Security/SecurityConfig.java
+++ pub/src/main/java/com/grupo_5/pub/Infra/Security/SecurityConfig.java
@@ -1,8 +1,11 @@
+package com.grupo_5.pub.Infra.Security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,20 +19,20 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 //Esses endpoints qualquer um pode acessar
-                .requestMatchers("/login").permitAll();
-                .requestMatchers("/cadastro").permitAll();
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/cadastro").permitAll()
 
                 //Esses endpoints estão exigindo que tenha uma autorização pra acessar
                 //Os ** na frente vão exigir que tudo que começa com o endpoint precisa estar autenticado
-                .requestMatchers("/ingredientes/**").authenticated();
-                .requestMatchers("/clientes/**").authenticated();
-                .requestMatchers("/comandas/**").authenticated();
-                .requestMatchers("/eventos/**").authenticated();
-                .requestMatchers("/bebidas/**").authenticated();
-                .requestMatchers("/mesas/**").authenticated();
-                .requestMatchers("/api/promocoes/**").authenticated();
+                .requestMatchers("/ingredientes/**").authenticated()
+                .requestMatchers("/clientes/**").authenticated()
+                .requestMatchers("/comandas/**").authenticated()
+                .requestMatchers("/eventos/**").authenticated()
+                .requestMatchers("/bebidas/**").authenticated()
+                .requestMatchers("/mesas/**").authenticated()
+                .requestMatchers("/api/promocoes/**").authenticated()
 
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
             )
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
